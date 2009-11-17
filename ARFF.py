67d98a5e92073f07d278394a9ce8d38238f4b276
Chris Johnson <effigies@gmail.com>
2009-11-17 14:10:01 -0600

    ARFF imports; PARF exports; wrapper

--- ARFF.py
+++ ARFF.py
@@ -7,11 +7,28 @@
 #  Copyright (c) 2009 The University of Tulsa. All rights reserved.
 #
 
+import csv
 import re
 
 def transpose(matrix):
 	"""Transpose a matrix. Assumes an m x n matrix."""
 	return [list(m) for m in zip(*matrix)]
+
+def genAttr(attribute):
+	if attribute[0] in ["'",'"']:
+		raise Exception("Complex attribute names not implemented")
+
+	name, attr = attribute.split(None,1)
+	if re.match("numeric",attr,re.I):
+		return (name, Numeric())
+	elif re.match("string",attr,re.I):
+		return (name, String())
+
+	if attr[0] == '{' and attr[-1] == '}':
+		return (name, Nominal(*(attr[1:-1].split(','))))
+
+	else:
+		raise Exception("Data attributes not implemented")
 
 # ARFF Attribute types
 class Attribute(object):
@@ -43,7 +60,7 @@ class Nominal(Attribute):
 		if value == "?":
 			return True
 		return value in self.valid
-		
+
 class String(Attribute):
 	def __init__(self, q="'"):
 		self.pattern = re.compile(r"%s(([^\\%s]|\\[^%s])*\\%s)*([^\\%s]|\\[^%s])*%s$|[^%s\s]\S*$" % (q,q,q,q,q,q,q,q))
@@ -58,7 +75,7 @@ class Date(Attribute):
 class ARFF:
 	p_rel = re.compile("@relation\s+",re.I)
 	p_attr = re.compile("@attribute\s+", re.I)
-	p_data = re.compile("@data\s+", re.I)
+	p_data = re.compile("@data\s*$", re.I)
 
 	def __init__(self, name = "", attributes = [], data = [[]], comments = []):
 		self.name = name
@@ -68,7 +85,7 @@ class ARFF:
 
 	def __str__(self):
 		output = []
-		output.extend(["%% %s" % comment for comment in self.comments])
+		output.extend(["%% %s" % comment for comment in self.comment])
 		output.append("@relation %s" % self.name)
 		output.extend(["@attribute %s %s" % attr for attr in self.attributes])
 		output.append("@data")
@@ -93,30 +110,57 @@ class ARFF:
 		elif not all([attr.match(val) for (_,attr), values in zip(self.attributes, dataT) for val in values]):
 			raise ValueError("Data rows may not have unexpected values.")
 
-		if not all([isinstance(comment,str) for comment in comments]):
+		if not all([isinstance(comment,str) for comment in self.comment]):
 			raise ValueError("Comments must be strings.")
 
 	def writeARFF(self,file):
 		file.write(self)
 
+	def writePARF(self,file):
+		output = []
+		output.extend(["%% %s" % comment for comment in self.comment])
+                output.append("@relation %s" % self.name)
+                output.extend(["@attribute %s %s" % attr for attr in self.attributes])
+                output.append("@data")
+		for row in self.data:
+			new_row = map(lambda x: "?" if x == "NA" else x, row)
+			output.extend([','.join(partial_row) for partial_row in self.splitrow(new_row)])
+		output.append("")
+		file.write('\n'.join(output))
+
+	def splitrow(self, row):
+		if len(row) < 500:
+			return [row]
+		return [row[:500] + ["&"]] + self.splitrow(row[500:])
+
 	def parseARFF(self,file):
-		row = file.readline()
-		while row.startswith('%'):
-			self.comments.append(row[1:-1])
-			row = file.readline()
-			while row == '\n':
-				row = file.readline()
+		for line in file:
+			line = line.strip()
+			if line == '':
+				continue
 
-		self.name = p_rel.sub("",row)
-			
-		while row[0] == '\n':
-			row = file.readline()
+			if line.startswith('%'):
+				self.comment.append(line[1:])
+			elif ARFF.p_rel.match(line):
+				self.name = ARFF.p_rel.sub("",line)
+			elif ARFF.p_attr.match(line):
+				self.attributes.append(genAttr(ARFF.p_attr.sub("",line)))
+			elif ARFF.p_data.match(line):
+				break
 
-		self.name
+		reader = csv.reader(file)
 
-def BioData:
+		self.data = [row for row in reader]
+
+		self.validate()
+
+class BioData:
 	def __init__(self, subjects = [], snps = [], genotypes = [], phenotypes = []):
 		self.subjects = subjects
 		self.snps = snps
 		self.genotypes = genotypes
 		self.phenotypes = phenotypes
+
+if __name__ == '__main__':
+	a = ARFF()
+	a.parseARFF(open("/Users/administrator/Downloads/dat2-final.arff",'r'))
