9151975eaaa45dede4e37adc87fd03291b094fba
serprex <junkmute@hotmail.com>
2009-12-06 22:01:11 +0000

    Generators are cool

--- ARFF.py
+++ ARFF.py
@@ -9,37 +9,32 @@
 
 import csv
 import re
+from itertools import chain,izip as zip
 
 def transpose(matrix):
 	"""Transpose a matrix. Assumes an m x n matrix."""
 	return [list(m) for m in zip(*matrix)]
 
 def genAttr(attribute):
-	if attribute[0] in ["'",'"']:
+	if attribute[0] in "'\"":
 		raise Exception("Complex attribute names not implemented")
 
 	name, attr = attribute.split(None,1)
 	if re.match("numeric",attr,re.I):
 		return (name, Numeric())
-	elif re.match("string",attr,re.I):
+	if re.match("string",attr,re.I):
 		return (name, String())
-
-	if attr[0] == '{' and attr[-1] == '}':
+	if attr[0]+attr[-1] == "{}":
 		return (name, Nominal(*(attr[1:-1].split(','))))
-
-	else:
-		raise Exception("Data attributes not implemented")
+	raise Exception("Data attributes not implemented")
 
 # ARFF Attribute types
 class Attribute(object):
 	"""Base attribute class. Will only match "?" without throwing an error."""
 	def match(self, value):
-		if value == "?":
-			return True
-		return re.match(self.pattern, value) is not None
-
+		return value=="?" or re.match(self.pattern, value) is not None
 	def __str__(self):
-		abstract
+		raise NotImplemented("abstract")
 
 class Numeric(Attribute):
 	"""Numeric attribute class. Matches integer or floating representations."""
@@ -53,13 +48,8 @@ class Nominal(Attribute):
 	def __init__(self, *args):
 		self.valid = args
 		
-	def __str__(self):
-		return "{" + ','.join(self.valid) + "}"
-		
-	def match(self, value):
-		if value == "?":
-			return True
-		return value in self.valid
+	def __str__(self):return "{" + ','.join(self.valid) + "}"
+	def match(self, value):return value=="?" or value in self.valid
 
 class String(Attribute):
 	def __init__(self, q="'"):
@@ -85,17 +75,16 @@ class ARFF:
 
 	def __str__(self):
 		output = []
-		output.extend(["%% %s" % comment for comment in self.comment])
+		output+=("%% %s" % comment for comment in self.comment)
 		output.append("@relation %s" % self.name)
-		output.extend(["@attribute %s %s" % attr for attr in self.attributes])
+		output+=("@attribute %s %s" % attr for attr in self.attributes)
 		output.append("@data")
-		output.extend([','.join(row) for row in self.data])
+		output+=(','.join(row) for row in self.data)
 		return '\n'.join(output)
 		
 	def validate(self):
 		if not String().match(self.name):
 			raise ValueError("ARFF relation name must be defined.")
-
 		if not all([isinstance(attr, tuple) for attr in self.attributes]):
 			raise TypeError("Attributes must be tuples.")
 		elif not all([isinstance(attr[0], str) for attr in self.attributes]):
@@ -109,37 +98,38 @@ class ARFF:
 			raise ValueError("Data rows must have same length as attributes.")
 		elif not all([attr.match(val) for (_,attr), values in zip(self.attributes, dataT) for val in values]):
 			raise ValueError("Data rows may not have unexpected values.")
-
-		if not all([isinstance(comment,str) for comment in self.comment]):
+		if not all(isinstance(comment,str) for comment in self.comment):
 			raise ValueError("Comments must be strings.")
 
-	def writeARFF(self,file):
-		file.write(self)
+	def writeARFF(self,file):file.write(self)
 
 	def writePARF(self,file):
 		output = []
-		output.extend(["%% %s" % comment for comment in self.comment])
-                output.append("@relation %s" % self.name)
-                output.extend(["@attribute %s %s" % attr for attr in self.attributes])
-                output.append("@data")
+		output+=("%% %s" % comment for comment in self.comment)
+		output.append("@relation %s" % self.name)
+		output+=("@attribute %s %s" % attr for attr in self.attributes)
+		output.append("@data")
 		for row in self.data:
-			new_row = map(lambda x: "?" if x == "NA" else x, row)
-			output.extend([','.join(partial_row) for partial_row in self.splitrow(new_row)])
+			output+=(','.join(partial_row) for partial_row in self.splitrow("?" if x=="NA" else x for x in row))
 		output.append("")
 		file.write('\n'.join(output))
 
 	def splitrow(self, row):
-		if len(row) < 500:
-			return [row]
+		if len(row) < 500:return [row]
 		return [row[:500] + ["&"]] + self.splitrow(row[500:])
+	def splitrow(self,row):#No longer recurses and now capable of using a generator
+		row=iter(row).next
+		r=[]
+		try:
+			while 1:r+=chain((row() for a in xrange(500)),"&")
+		except StopIteration:return r
 
 	def parseARFF(self,file):
 		for line in file:
 			line = line.strip()
-			if line == '':
-				continue
+			if not line:continue
 
-			if line.startswith('%'):
+			if line[0]=='%':
 				self.comment.append(line[1:])
 			elif ARFF.p_rel.match(line):
 				self.name = ARFF.p_rel.sub("",line)
@@ -149,9 +139,7 @@ class ARFF:
 				break
 
 		reader = csv.reader(file)
-
 		self.data = [row for row in reader]
-
 		self.validate()
 
 class BioData:
